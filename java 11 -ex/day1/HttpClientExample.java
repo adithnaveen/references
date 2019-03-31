85ad1ca618dae586a83be39ac6079593a9e17e9f
Naveen <adith.naveen@gmail.com>
2019-03-31 09:40:08 +0530

    changes in day1 http client

--- java 11 -ex/day1/HttpClientExample.java
+++ java 11 -ex/day1/HttpClientExample.java
@@ -20,18 +20,23 @@ import java.util.stream.Collectors;
 // http CRUD Operations 
 public class HttpClientExample {
 
+	private static String getTMS="https://trainingmanagementapp.herokuapp.com/api/courses/5c9f468e13f3a52110b3793e"; 
+	
+	
 	public static void main(String[] args) throws Exception {
-//        httpGetRequest();
-//        httpPostRequest();
-//		asynchronousGetRequest();
-		asynchronousMultipleRequests();
-//        pushRequest();
+//		httpGetRequest();
+        	httpPostRequest();
+//			asynchronousGetRequest();
+		// dont try this 
+//			asynchronousMultipleRequests();
+		// dont try 
+//        	pushRequest();
 	}
 
 	public static void httpGetRequest() throws URISyntaxException, IOException, InterruptedException {
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder().version(HttpClient.Version.HTTP_1_1)
-				.uri(URI.create("http://jsonplaceholder.typicode.com/posts/1"))
+				.uri(URI.create("https://trainingmanagementapp.herokuapp.com/api/courses/5c9f468e13f3a52110b3793e"))
 //            Sends "Accept-Encoding: gzip,deflate" request header and 
 //            decompresses based on Content-Encoding (configurable) Redirects 
 //            are no longer followed by default.
@@ -50,7 +55,8 @@ public class HttpClientExample {
 		HttpRequest request = HttpRequest.newBuilder(new URI("http://jsonplaceholder.typicode.com/posts"))
 				.version(HttpClient.Version.HTTP_2)
 //            .POST(BodyPublishers.ofString("Sample Post Request - Naveen "))
-				.POST(BodyPublishers.ofString("{\r\n" + "\"userId\": 101,\r\n" + "\"id\": 1,\r\n"
+				// this will give always 101 as there are already 100 records 
+				.POST(BodyPublishers.ofString("{\r\n" + "\"userId\": 10,\r\n" + "\"id\": 1,\r\n"
 						+ "\"title\": \"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\",\r\n"
 						+ "\"body\": \"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\"\r\n"
 						+ "}"))
@@ -66,8 +72,7 @@ public class HttpClientExample {
 			URI httpURI = new URI("http://jsonplaceholder.typicode.com/posts/1");
 			HttpRequest request = HttpRequest.newBuilder(httpURI).version(HttpClient.Version.HTTP_2).build();
 
-			CompletableFuture<Void> futureResponse = client.sendAsync(request, 
-					HttpResponse.BodyHandlers.ofString())
+			CompletableFuture<Void> futureResponse = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
 					.thenAccept(resp -> {
 
 						System.out.println("Got pushed response " + resp.uri());
@@ -75,7 +80,7 @@ public class HttpClientExample {
 						System.out.println("Response body: " + resp.body());
 					});
 			System.out.println("futureResponse" + futureResponse);
-			System.out.println(futureResponse.isDone());
+			System.out.println("isDone status " +futureResponse.isDone());
 
 			// this method will not leave the scope till the response is got
 			futureResponse.get();
@@ -88,29 +93,19 @@ public class HttpClientExample {
 	}
 
 	public static void asynchronousMultipleRequests() throws URISyntaxException {
-			HttpClient client = HttpClient.newHttpClient();
-			List<URI> uris = Arrays.asList(
-					new URI("http://jsonplaceholder.typicode.com/posts/1"),
-					new URI("http://jsonplaceholder.typicode.com/posts/2"));
+		HttpClient client = HttpClient.newHttpClient();
+		List<URI> uris = Arrays.asList(new URI("http://jsonplaceholder.typicode.com/posts/1"),
+				new URI("http://jsonplaceholder.typicode.com/posts/2"));
 
-			List<HttpRequest> requests = uris.stream().map(HttpRequest::newBuilder).
-					map(reqBuilder -> reqBuilder.build())
-					.collect(Collectors.toList());
+		List<HttpRequest> requests = uris.stream().map(HttpRequest::newBuilder).map(reqBuilder -> reqBuilder.build())
+				.collect(Collectors.toList());
 
-			System.out.println("Got pushed response1 " + requests);
+		System.out.println("Got pushed response1 " + requests);
 
-			requests.forEach(System.out :: println);
-			
+		requests.forEach(System.out::println);
 
-
-			CompletableFuture.allOf(
-					requests.stream().map(
-							request -> client.sendAsync(
-							request, BodyHandlers.ofString()))
-							.toArray(CompletableFuture<?>[]::new)).
-							thenAccept(System.out::println).join(); 
-			
-
+		CompletableFuture.allOf(requests.stream().map(request -> client.sendAsync(request, BodyHandlers.ofString()))
+				.toArray(CompletableFuture<?>[]::new)).thenAccept(System.out::println).join();
 
 		System.out.println("Some other business logic");
 	}
@@ -129,7 +124,8 @@ public class HttpClientExample {
 		// pushPromiseRequest,
 		// Function<HttpResponse.BodyHandler<T>,?CompletableFuture<HttpResponse<T>>>
 		// acceptor)
-		httpClient.sendAsync(pageRequest, BodyHandlers.ofString(), pushPromiseHandler()).thenAccept(pageResponse -> {
+		httpClient.sendAsync(pageRequest, BodyHandlers.ofString(), pushPromiseHandler()).
+			thenAccept(pageResponse -> {
 			System.out.println("Page response status code: " + pageResponse.statusCode());
 			System.out.println("Page response headers: " + pageResponse.headers());
 			String responseBody = pageResponse.body();
@@ -140,8 +136,11 @@ public class HttpClientExample {
 	}
 
 	private static PushPromiseHandler<String> pushPromiseHandler() {
-		return (HttpRequest initiatingRequest, HttpRequest pushPromiseRequest,
-				Function<HttpResponse.BodyHandler<String>, CompletableFuture<HttpResponse<String>>> acceptor) -> {
+		return (HttpRequest initiatingRequest, 
+				HttpRequest pushPromiseRequest,
+				Function<HttpResponse.BodyHandler<String>, 
+				CompletableFuture<HttpResponse<String>>> acceptor) -> {
+					
 			acceptor.apply(BodyHandlers.ofString()).thenAccept(resp -> {
 				System.out.println(" Pushed response: " + resp.uri() + ", headers: " + resp.headers());
 			});
